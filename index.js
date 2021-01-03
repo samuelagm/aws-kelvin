734f782fbfd2654746b53deecefdd13848938617
Samuel Agbonkpolo <samuelagm@gmail.com>
2021-01-03 02:54:09 +0100

    added a little instruction

--- index.js
+++ index.js
@@ -45,5 +45,8 @@ app.use(
     graphiql: true,
   })
 );
+app.use("/", (i, o) => {
+  o.json({ message: "head to /graphql" });
+});
 app.listen(4000);
 console.log("Running a GraphQL API server at http://localhost:4000/graphql");
